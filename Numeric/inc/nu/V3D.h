dd3fa2b5b4b693d0cd7292cb100d65c1379ac3e4
unknown <All@black.(none)>
2011-10-27 20:26:25 +0100

    A fix

--- Numeric/inc/nu/V3D.h
+++ Numeric/inc/nu/V3D.h
@@ -14,11 +14,11 @@ namespace nu
     V3D():m_x(),m_y(),m_z(){}
     V3D(const double& x,const double& y,const double& z):m_x(x),m_y(y),m_z(z){}
     V3D(const V3D& v):m_x(v.m_x),m_y(v.m_y),m_z(v.m_z){}
-    V3D& operator=(const V3D& v){m_x=v.m_x;m_y=v.m_y;m_z=v.m_z;}
+    V3D& operator=(const V3D& v){m_x=v.m_x;m_y=v.m_y;m_z=v.m_z;return *this;}
     const double& x() const {return m_x;}
     const double& y() const {return m_y;}
     const double& z() const {return m_z;}
-    void setX() {m_x = x;}
+    void setX(const double& x) {m_x = x;}
     void setY(const double& y) {m_y = y;}
     void setZ(const double& z) {m_z = z;}
   private:
